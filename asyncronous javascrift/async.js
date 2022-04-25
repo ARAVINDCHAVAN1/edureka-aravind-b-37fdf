0ca2665d8fe4b3edd6034acd9c3f9207dc51820b
DEll <chavanaravind2000@gmail.com>
2022-04-25 14:59:28 +0530

    async

---
+++ asyncronous javascrift/async.js
@@ -0,0 +1,8 @@
+console.log("Task 1");
+
+ setTimeout( ()=>{console.log("Task 2")},1000);
+ setTimeout( ()=>{console.log("Task 6")},0);
+
+console.log("Task 3");
+
+console.log("Task 4");
---
+++ asyncronous javascrift/sync.js
@@ -0,0 +1,7 @@
+console.log("Task 1");
+
+console.log("Task 2");
+
+console.log("Task 3");
+
+console.log("Task 4");
